f60d1113ae60d1356e4d7126382edc56e4145aab
spy-lock <michaelfeenstra29@gmail.com>
2023-06-26 10:20:20 +0200

    Update Start-Deployment.ps1

--- Start-Deployment.ps1
+++ Start-Deployment.ps1
@@ -1,21 +1,8 @@
-#  checks if the current user is an administrator and if not, it elevates the privileges by running the script again with the Runas verb
-IF (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
-    #  checks if the operating system build number is greater than or equal to 6000
-    IF ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-        #  constructs the command line arguments for running the script with the same path and parameters as the current invocation
-        $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
-        #  starts a new process of PowerShell.exe with the Runas verb and the command line arguments and maximizes the window
-        Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine -WindowStyle Maximized
-        #  exits the current script
-        Exit
-    }
-}
-
 Write-Output "Copying item..."
 Write-Output ""
 Start-Sleep -Seconds 2
 New-Item -Type Directory -Path C:/ -Name files -Force -ErrorAction SilentlyContinue | Out-Null
-copy-item ./enrollment/drivers, ./enrollent/scripts, ./enrollment/winform -Destination C:/files -Recurse -ErrorAction SilentlyContinue -Force | Out-Null
+copy-item ./enrollment/drivers, ./enrollment/scripts, ./enrollment/winform -Destination C:/files -Recurse -ErrorAction SilentlyContinue -Force | Out-Null
 
 Write-Output "Items coppied."
 Write-Output ""
