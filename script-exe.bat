cf4c1581f381e93bec1db412b863b665f45590b6
spy-lock <michaelfeenstra29@gmail.com>
2023-06-26 18:53:20 +0200

    Update script-exe.bat

--- script-exe.bat
+++ script-exe.bat
@@ -1,2 +1,2 @@
-powershell start-process powershell.exe -argumentlist {set-executionpolicy -scope currentuser bypass} -verb runas
+powershell start-process powershell.exe -argumentlist {set-executionpolicy -scope currentuser bypass -force} -verb runas
 powershell start-process powershell -argumentlist ./start-deployment.ps1 -WindowStyle Maximized
