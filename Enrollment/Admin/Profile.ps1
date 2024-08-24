410ddd237b85a194b4b6ee9a019375663d1e9e48
spy-lock <michaelfeenstra29@gmail.com>
2024-08-24 15:12:41 +0200

    upgrade for Windows Home

--- Enrollment/Admin/Profile.ps1
+++ Enrollment/Admin/Profile.ps1
@@ -1,35 +1,43 @@
 Function Set-WinImage {
 
-    Remove-Item $env:ProgramFiles\enrollment\mount\WIN11_en -Force -Recurse -erroraction SilentlyContinue
+    Remove-Item $env:ProgramFiles\enrollment\mount\WIN11PRO_en -Force -Recurse -erroraction SilentlyContinue
+    Remove-Item $env:ProgramFiles\enrollment\mount\WIN11HOME_en -Force -Recurse -erroraction SilentlyContinue
 
     #CREATES DIRECTORIES FOR WHERE THE MOUNTED IMAGES WILL BE MOUNTED
     Write-Output "`n Creating directories..."
     WRITE-Output ""
-    New-Item -ItemType Directory $env:ProgramFiles\enrollment\mount\Win11_en -Force -ErrorAction SilentlyContinue | Out-Null
+    New-Item -ItemType Directory $env:ProgramFiles\enrollment\mount\Win11PRO_en -Force -ErrorAction SilentlyContinue | Out-Null
+    New-Item -ItemType Directory $env:ProgramFiles\enrollment\mount\Win11HOME_en -Force -ErrorAction SilentlyContinue | Out-Null
     Write-Output ""
     start-sleep -second 1
     #MOUNTS THE IMAGES TO THE CREATED DIRECTORIES
     write-Output "`n Mounting images..."
     WRITE-output ""
-    dism /mount-image /imagefile:"C:\program files\enrollment\iso\images\WIN11_en-gb\sources\install.wim" /index:1 /mountdir:"C:\program files\enrollment\mount\win11_en"
+    dism /mount-image /imagefile:"C:\program files\enrollment\iso\images\WIN11_en-gb\sources\install.wim" /index:1 /mountdir:"C:\program files\enrollment\mount\win11PRO_en"
+    dism /mount-image /imagefile:"C:\program files\enrollment\iso\images\WIN11_en-gb\sources\install.wim" /index:2 /mountdir:"C:\program files\enrollment\mount\win11HOME_en"
 
     write-Output "`n Removing files in images..."
     WRITE-Output ""
 
-    Remove-Item $env:ProgramFiles\enrollment\mount\win11_en\files -Force -Recurse
+    Remove-Item $env:ProgramFiles\enrollment\mount\win11PRO_en\files -Force -Recurse
+    Remove-Item $env:ProgramFiles\enrollment\mount\win11HOME_en\files -Force -Recurse
 
     #COPYING FILES INSIDE THE MOUNTED IMAGES
     write-host "`n Copying items now..."
-    copy-files -Source $env:ProgramFiles\enrollment\files -Destination $env:ProgramFiles\enrollment\mount\win11_en\files -Activity "Copying 'WIN11_en\files'..."  -Verbose
+    copy-files -Source $env:ProgramFiles\enrollment\files -Destination $env:ProgramFiles\enrollment\mount\win11PRO_en\files -Activity "Copying 'WIN11PRO_en\files'..."  -Verbose
+    copy-files -Source $env:ProgramFiles\enrollment\files -Destination $env:ProgramFiles\enrollment\mount\win11HOME_en\files -Activity "Copying 'WIN11HOME_en\files'..."  -Verbose
+
     WRITE-Output ""
 
 
     #UNMOUNTING IMAGES AND SAVING CHANGES
     write-Output "`n Unmounting images..."
     WRITE-Output ""
-    dism /unmount-image /mountdir:"C:\program files\enrollment\mount\win11_en" /commit
+    dism /unmount-image /mountdir:"C:\program files\enrollment\mount\win11PRO_en" /commit
+    dism /unmount-image /mountdir:"C:\program files\enrollment\mount\win11HOME_en" /commit
 
-    Remove-Item $env:ProgramFiles\enrollment\mount\WIN11_en -Force -Recurse -erroraction SilentlyContinue
+    Remove-Item $env:ProgramFiles\enrollment\mount\WIN11PRO_en -Force -Recurse -erroraction SilentlyContinue
+    Remove-Item $env:ProgramFiles\enrollment\mount\WIN11HOME_en -Force -Recurse -erroraction SilentlyContinue
 
     Read-Host -Prompt "press (ENTER) to exit"
 }
@@ -298,13 +306,11 @@ Dism /Add-Package /Image:"C:\program files\enrollment\PE\mount" /PackagePath:"C:
 
 Dism /Unmount-Image /MountDir:"C:\program files\enrollment\PE\mount" /Commit
 
-dism /Apply-Image /ImageFile:"C:\program files\enrollment\PE\media\sources\boot.wim" /Index:1 /ApplyDir:P:\
+Start-Process powershell -ArgumentList {makewinpemedia /UFD "C:\program files\enrollment\PE\mount" P:} -WorkingDirectory "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools" -Wait
 
-Write-Output "`n Adding boot files..."
+Write-Output "`n creating WinPE drive..."
 Write-Output ""
 Start-Sleep -Seconds 2
-
-Start-Process powershell -ArgumentList {bcdboot P:\Windows /s P: /f ALL} -WorkingDirectory "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools" -Wait
 
 copy-item -path 'C:\Program Files\enrollment\Files\checkdrive.ps1','C:\Program Files\enrollment\Files\loading-screen.ps1','C:\Program Files\enrollment\Files\TPM_Fix.cmd'  -Destination P:/ -Force
 Copy-Item -Path 'C:\Program Files\enrollment\Files\startnet.cmd' -Destination P:\windows\system32 -Force -ErrorAction SilentlyContinue
